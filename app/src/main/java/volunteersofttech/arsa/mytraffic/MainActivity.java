01a7eda8fc02697f6b1255dbb32c72f72c280839
arsa <arsa_ko@hotmail.com>
2016-02-20 14:02:48 +0700

    ประการตัวแปรไว้แล้ว

--- app/src/main/java/volunteersofttech/arsa/mytraffic/MainActivity.java
+++ app/src/main/java/volunteersofttech/arsa/mytraffic/MainActivity.java
@@ -2,12 +2,19 @@ package volunteersofttech.arsa.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ประการ ตัวแปร
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    } //main method
+} //main class นี้คืน class หลัก
